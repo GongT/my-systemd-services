b8b0a2ee7011f3383e9efc3536d409827719fc1f
GongT <admin@gongt.me>
2019-02-11 17:35:37 +0800

    update wg

--- network/wire-guard/reload-script.sh
+++ network/wire-guard/reload-script.sh
@@ -1,11 +1,35 @@
 set -e
-CACHE=/etc/wireguard/last-ip.txt
-echo -n "resolving ip address..."
-IP_ADDR=$(nslookup home.gongt.me ns.he.net  | grep "Address: " | grep -oP "[\.\d]+")
-echo $IP_ADDR
-if [ -e "$CACHE" ] && [ "$(< "$CACHE")" = "$IP_ADDR" ] ; then
+
+NETDEV=30-wg0
+NETDEV_FILE="/etc/systemd/network/$NETDEV.netdev"
+HOSTNAME=home.gongt.me
+RESOLVER=ns.he.net
+
+INTERFACE=$(cat "$NETDEV_FILE" | grep 'Name' | grep -oE '\S+$')
+PORT=$(cat "$NETDEV_FILE" | grep 'Endpoint' | grep -oE ':\S+$')
+PUBLIC_KEY="$(wg show wg0 | grep 'peer:' | grep -oE '\S+$')"
+
+CURRENT="$(wg show wg0 | grep 'endpoint' | grep -oE '\b([0-9]+\.)+[0-9]+\b')"
+echo "Current ip address is: $CURRENT."
+
+echo -n "Resolving ip address: "
+IP_ADDR="$(nslookup "$HOSTNAME" "$RESOLVER" | grep 'Address: ' | grep -oE '\b([0-9]+\.)+[0-9]+\b')"
+echo "$IP_ADDR."
+
+if [ "$CURRENT" = "$IP_ADDR" ] ; then
 	echo "IP address is same."
-	exit
+	if echo "$*" | grep -q -- "-f" ; then
+		echo "Force run"
+	else
+		exit 0
+	fi
 fi
-systemctl restart systemd-networkd.service
-echo "$IP_ADDR" > "$CACHE"
+
+echo "Updating network device config file ($NETDEV_FILE)..."
+sed -Ei "s/^Endpoint\s*=\s*.+$/Endpoint = $IP_ADDR$PORT/g" "$NETDEV_FILE"
+
+echo "Set wg0 peer IP address ($PUBLIC_KEY)..."
+wg set "$INTERFACE" peer "$PUBLIC_KEY" endpoint "$IP_ADDR$PORT"
+
+echo "Completed..."
+
